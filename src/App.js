f48f2a36ea1bcd897c8879388a2dd546125a8eb5
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2022-11-16 19:02:02 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  // const [num, setnum] = useState(0);
+  // const add = () => {
+  //   setnum(num + 1);
+  // };
+  // const min = () => {
+  //   if (num > 0) setnum(num - 1);
+  // };
+  // const todostd = [
+  //   { name: "Sultan", id: 0, iscompleted: false },
+  //   { name: "Sulaiman", id: 1, iscompleted: true },
+  //   { name: "Abdulhakeem", id: 2, iscompleted: false },
+  //   { name: "Ali", id: 3, iscompleted: true },
+  // ];
+  const [list, setlist] = useState([
+    { name: "Sultan", id: 0, Hero: true },
+    { name: "Ahmed", id: 1, Hero: true },
+    { name: "Naed", id: 2, Hero: true },
+    { name: "John", id: 3, Hero: false },
+  ]);
+  const add = (e) => {
+    e.preventDefault();
+    let inp = e.target.input.value;
+    setlist([...list, { name: inp, id: list.length + 1, hero: true }]);
+  };
+  const remove = (del) => {
+    setlist(list.filter((u) => u.id !== del));
+  };
+
+  return (
+    <>
+      {/* {<button onClick={add}>+</button>}
+      {<button onClick={min}>-</button>}
+      {<h1>{num}</h1>} */}{" "}
+      {
+        <>
+          <form onSubmit={add}>
+            {" "}
+            <input name="input" placeholder="add"></input>
+            <button>add new</button>
+          </form>
+        </>
+      }
+      <ul>
+        {list.map((e, i) => {
+          return (
+            <>
+             
+                <h2 key={i} >
+                  {e.name}
+                </h2>
+                {<button onClick={() =>remove(i)}>del</button>}
+              {console.log(i)}
+            </>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default App;
